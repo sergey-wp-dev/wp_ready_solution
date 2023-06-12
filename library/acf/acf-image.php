2640562342343a8bd01fd7fc96e2b746fa590912
Sergey Bezdudnyy <sergey@auq.io>
2023-06-12 12:51:46 +0300

    Add ACF image display with image size

--- library/acf/acf-image.php
+++ library/acf/acf-image.php
@@ -7,3 +7,15 @@
 ?>
 	<img class="img-fluid" src="<?php echo $img['url']; ?>" alt="<?php echo $img['alt']; ?>">
 <?php endif ?>
+
+
+<?php
+// ACF image array display with image size
+?>
+<?php
+	if( !empty( $img ) ) :
+		$img_url = $img['sizes']['medium'];
+		$img_alt = $img['alt'];
+?>
+	<img src="<?php echo esc_url($img_url); ?>" class="img-fluid" alt="<?php echo esc_attr($img_alt); ?>">
+<?php endif; ?>
